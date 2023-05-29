e47233f18bad3dd72666f29a188902aa2d24d71b
charliejp21 <charliejp21@gmail.com>
2023-05-28 18:14:43 -0600

    Iniciado un proyecto

---
+++ index.js
@@ -0,0 +1,11 @@
+//conexion a la base de datos
+
+//crear servidor de node
+
+//configurar cors
+
+//Convertir los datos del body a js
+
+//Cargar y configurar rutas 
+
+//poner servidor a esuchar peticiones Http

08e3b3055f76fa72e5ab899153fa91688393e64d
charliejp21 <charliejp21@gmail.com>
2023-12-07 00:40:19 -0600

    Ruta de actualizar imagen de publicación terminada

--- routes/publicationRouter.js
+++ routes/publicationRouter.js
@@ -1,12 +1,29 @@
 const {Router} = require('express');
-const {savePublicationHandler, getPublicationHandler,deletePublicationHandler, getPublicationsHandler} = require('../handlers/publicationHandler')
+const {savePublicationHandler, getPublicationHandler,deletePublicationHandler, getPublicationsHandler, uploadImgPublicationHandler} = require('../handlers/publicationHandler')
 const {auth} = require("../middlewares/auth")
-
+const multer = require("multer")
 const publicationRoutes = Router();
+
+
+const storage = multer.diskStorage({
+
+    destination: (req, res, cb) => {
+
+        cb(null, "./uploads/publications")
+    },
+    filename:(req, file, cb) =>{
+
+        cb(null, "publication-image-" + Date.now() + "-" + file.originalname)
+
+    }
+})
+
+const uploads = multer({storage});
 
 publicationRoutes.post("/save/", auth, savePublicationHandler)
 publicationRoutes.get("/detail/:id", auth, getPublicationHandler)
 publicationRoutes.delete("/id/:id", auth, deletePublicationHandler)
 publicationRoutes.get("/user/:userId/:page?", auth, getPublicationsHandler)
+publicationRoutes.put("/upload-img/:idPublication", [auth, uploads.single("file0")], uploadImgPublicationHandler)
 
 module.exports = publicationRoutes;

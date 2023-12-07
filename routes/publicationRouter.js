46d450229892fa06da84aa352491097ba12bc503
charliejp21 <charliejp21@gmail.com>
2023-12-07 01:08:01 -0600

    Ruta de obtener imagen terminada

--- routes/publicationRouter.js
+++ routes/publicationRouter.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const {savePublicationHandler, getPublicationHandler,deletePublicationHandler, getPublicationsHandler, uploadImgPublicationHandler} = require('../handlers/publicationHandler')
+const {savePublicationHandler, getPublicationHandler,deletePublicationHandler, getPublicationsHandler, uploadImgPublicationHandler, getImagePublicationHandler} = require('../handlers/publicationHandler')
 const {auth} = require("../middlewares/auth")
 const multer = require("multer")
 const publicationRoutes = Router();
@@ -25,5 +25,6 @@ publicationRoutes.get("/detail/:id", auth, getPublicationHandler)
 publicationRoutes.delete("/id/:id", auth, deletePublicationHandler)
 publicationRoutes.get("/user/:userId/:page?", auth, getPublicationsHandler)
 publicationRoutes.put("/upload-img/:idPublication", [auth, uploads.single("file0")], uploadImgPublicationHandler)
+publicationRoutes.get("/image/:nameImage", auth, getImagePublicationHandler)
 
 module.exports = publicationRoutes;
